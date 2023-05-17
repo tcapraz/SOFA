1370a43e4ddccb0bfbe6d31e1782a2cfbd3238f0
capraz <tuemay.capraz@embl.com>
2023-05-17 14:35:14 +0200

    add pbmc5k example

--- spFA/spFA.py
+++ spFA/spFA.py
@@ -18,7 +18,7 @@ def npsigmoid(x):
 
 
 class spFA:
-    def __init__(self, X, llh, num_factors, views, y=None, target_llh=None, supervised_factors=0, device=torch.device("cpu"), ard=True, horseshoe=True, update_freq=200):
+    def __init__(self, X, llh, num_factors, views, y=None, target_llh=None, design=None, supervised_factors=0, device=torch.device("cpu"), ard=True, horseshoe=True, update_freq=200, subsample=0):
         """
 
 
@@ -68,8 +68,21 @@ class spFA:
 
         if self.target_llh == "multinomial":
             self.k = len(np.unique(y.numpy()))
-
-    def sFA_model(self):
+            
+        if torch.is_tensor(y):
+            if len(y.shape) > 1:
+                self.y_dim = y.shape[1]
+            else:
+                self.y_dim = 1
+                
+        if not torch.is_tensor(design) & torch.is_tensor(y):
+            self.design = torch.eye(self.y_dim, device=device)
+        else:
+            self.design = design.to(device)
+        self.num_samples = X[0].shape[0]
+        self.subsample = subsample
+        
+    def sFA_model(self, subsample=32):
         X = self.X
         y = self.y
         llh = self.llh
@@ -82,7 +95,7 @@ class spFA:
         num_samples = X[0].shape[0]
         num_features = [i.shape[1] for i in X]
 
-        sigma_data = pyro.param("sigma_data", torch.ones(1, device=device), constraint=pyro.distributions.constraints.positive)
+        sigma_data = pyro.param("sigma_data", torch.ones(num_views, device=device), constraint=pyro.distributions.constraints.positive)
         sigma_response = pyro.param("sigma_response", torch.ones(1, device=device), constraint=pyro.distributions.constraints.positive)
         if self.horseshoe:
             tau = pyro.sample("tau", dist.HalfCauchy(torch.ones(1, device=device)))
@@ -96,7 +109,7 @@ class spFA:
                 else:
                     W_ = pyro.sample("W_unshrunk_{}".format(i), dist.Normal(torch.zeros(num_features[i], device=device), torch.ones(num_features[i], device=device)).to_event(1))
                 if self.horseshoe:
-                    lam = pyro.sample("lam_{}".format(i), dist.HalfCauchy(torch.ones(num_features[i], device=device)).to_event(1))
+                    lam = pyro.sample("lam_{}".format(i), dist.HalfCauchy(torch.ones(num_features[i], device=device)*10).to_event(1))
                     W_ = pyro.deterministic("W_{}".format(i), W_ * lam**2 * tau**2)
                 else:
                     W_ = pyro.deterministic("W_{}".format(i), W_)
@@ -109,16 +122,24 @@ class spFA:
                     beta = pyro.sample("beta", dist.Normal(torch.zeros(self.k, device=device), torch.ones(self.k, device=device)).to_event(1))
 
                 else:
-                    beta = pyro.sample("beta", dist.Normal(torch.zeros(1, device=device), torch.ones(1, device=device)))
-
-        with pyro.plate("data", num_samples):
+                    beta = pyro.sample("beta", dist.Normal(torch.zeros(self.y_dim, device=device), torch.ones(self.y_dim, device=device)).to_event(1))
+                    
+        if subsample > 0:
+            data_plate = pyro.plate("data", num_samples, subsample_size=subsample)
+        else:
+            data_plate = pyro.plate("data", num_samples)
+            
+        with data_plate as ind:
+            ind = ind.flatten()
             Z = pyro.sample("Z", dist.Normal(torch.zeros(num_factors, device=device), torch.ones(num_factors, device=device)).to_event(1))
+
+
 
             X_pred = []
             for i in range(num_views):
                 if llh[i] == "bernoulli":
                     X_pred.append(Z @ W[i])
-                    pyro.sample("obs_data_{}".format(i), dist.Bernoulli(sigmoid(X_pred[i])).to_event(1), obs=X[i])
+                    pyro.sample("obs_data_{}".format(i), dist.Bernoulli(sigmoid(X_pred[i])).to_event(1), obs=X[i][ind,:])
                 else:
                     for j in range(num_factors):
                         X_ij = pyro.deterministic(f"X_{i}{j}", Z[:, [j]] @ W[i][[j], :])
@@ -126,20 +147,20 @@ class spFA:
 
                     X_pred.append(X_i)
 
-                    pyro.sample("obs_data_{}".format(i), dist.Normal(X_i, sigma_data).to_event(1), obs=X[i])
+                    pyro.sample("obs_data_{}".format(i), dist.Normal(X_i, sigma_data[i]).to_event(1), obs=X[i][ind,:])
 
             if supervised_factors > 0:
-                y_pred = Z[:, 0 : self.supervised_factors] @ beta
+                y_pred = Z[:, 0 : self.supervised_factors] @ torch.mul(beta, self.design)
                 if self.target_llh == "bernoulli":
-                    pyro.sample("obs_response", dist.Bernoulli(sigmoid(y_pred)).to_event(1), obs=y)
+                    pyro.sample("obs_response", dist.Bernoulli(sigmoid(y_pred)).to_event(1), obs=y[ind])
 
                 elif self.target_llh == "multinomial":
                     y_pred = softmax(y_pred)
-                    pyro.sample("obs_response", dist.Categorical(y_pred).to_event(1), obs=y)
+                    pyro.sample("obs_response", dist.Categorical(y_pred).to_event(1), obs=y[ind])
                 else:
-                    pyro.sample("obs_response", dist.Normal(y_pred, sigma_response).to_event(1), obs=y)
+                    pyro.sample("obs_response", dist.Normal(y_pred, sigma_response).to_event(1), obs=y[ind])
 
-    def sFA_guide(self):
+    def sFA_guide(self, subsample=32):
         X = self.X
         y = self.y
         llh = self.llh
@@ -162,10 +183,10 @@ class spFA:
                 with pyro.plate("betas", supervised_factors):
                     beta = pyro.sample("beta", dist.Normal(beta_loc, beta_scale).to_event(1))
             else:
-                beta_loc = pyro.param("beta_loc", torch.zeros(supervised_factors, device=device))
-                beta_scale = pyro.param("beta_scale", torch.ones(supervised_factors, device=device), constraint=pyro.distributions.constraints.positive)
+                beta_loc = pyro.param("beta_loc", torch.zeros(supervised_factors, self.y_dim, device=device))
+                beta_scale = pyro.param("beta_scale", torch.ones(supervised_factors, self.y_dim, device=device), constraint=pyro.distributions.constraints.positive)
                 with pyro.plate("betas", supervised_factors):
-                    beta = pyro.sample("beta", dist.Normal(beta_loc, beta_scale))
+                    beta = pyro.sample("beta", dist.Normal(beta_loc, beta_scale).to_event(1))
 
         if self.horseshoe:
             tau_loc = pyro.param("tau_loc", torch.ones(1, device=device), constraint=dist.constraints.positive)
@@ -174,6 +195,7 @@ class spFA:
         if self.ard:
             gamma_alpha = []
             gamma_beta = []
+     
         W_loc = []
         W_scale = []
 
@@ -196,10 +218,14 @@ class spFA:
                     W = pyro.sample("W_unshrunk_{}".format(i), dist.Normal(W_loc[i], W_scale[i]).to_event(1))
                 if self.horseshoe:
                     lam = pyro.sample("lam_{}".format(i), dist.Delta(lam_loc[i]).to_event(1))
-
-        with pyro.plate("data", num_samples):
-            pyro.sample("Z", dist.Normal(Z_loc, Z_scale).to_event(1))
-
+        if subsample > 0:
+            data_plate = pyro.plate("data", num_samples, subsample_size=subsample)
+        else:
+            data_plate = pyro.plate("data", num_samples)
+            
+        with data_plate as ind:
+            pyro.sample("Z", dist.Normal(Z_loc[ind,:], Z_scale[ind,:]).to_event(1))
+     
     def fit_spFA(self, n_steps=3000, lr=0.005, refit=False):
         """
         method to fit the spFA model
@@ -232,7 +258,7 @@ class spFA:
         last_elbo = np.inf
         # do gradient steps
         for step in pbar:
-            loss = self.svi.step()
+            loss = self.svi.step(subsample=self.subsample)
             # track loss
             self.history.append(loss)
 
@@ -242,7 +268,7 @@ class spFA:
                 last_elbo = loss
 
         self.isfit = True
-
+        
     def get_factors(self):
         """
         Sample factors from posterior
@@ -252,7 +278,7 @@ class spFA:
         Z_pred : numpy array
         """
         predictive = Predictive(self.sFA_model, guide=self.sFA_guide, num_samples=50, return_sites=["Z"])
-        samples = predictive()
+        samples = predictive(subsample=0)
         Z_pred = np.mean(samples["Z"].cpu().numpy(), axis=0)
         return Z_pred
 
@@ -275,12 +301,12 @@ class spFA:
             W_pred = []
             for i in range(len(self.X)):
                 predictive = Predictive(self.sFA_model, guide=self.sFA_guide, num_samples=50, return_sites=[f"W_{i}"])
-                samples = predictive()
+                samples = predictive(subsample=0)
                 W_pred.append(np.mean(samples[f"W_{i}"].cpu().numpy(), axis=0))
             return W_pred
         else:
             predictive = Predictive(self.sFA_model, guide=self.sFA_guide, num_samples=50, return_sites=[f"W_{view}"])
-            samples = predictive()
+            samples = predictive(subsample=0)
             return np.mean(samples[f"W_{view}"].cpu().numpy(), axis=0)
 
     def get_Xpred(self, view=None):
@@ -304,13 +330,13 @@ class spFA:
             X_pred = []
             for i in range(len(self.X)):
                 predictive = Predictive(self.sFA_model, guide=self.sFA_guide, num_samples=50, return_sites=[f"X_{i}"])
-                samples = predictive()
+                samples = predictive(subsample=0)
 
                 X_pred.append(np.mean(samples[f"X_{i}"].cpu().numpy(), axis=0))
             return X_pred
         else:
             predictive = Predictive(self.sFA_model, guide=self.sFA_guide, num_samples=50, return_sites=[f"X_{view}"])
-            samples = predictive()
+            samples = predictive(subsample=0)
             X_pred = np.mean(samples[f"X_{view}"].cpu().numpy(), axis=0)
             return X_pred
 
@@ -330,21 +356,21 @@ class spFA:
         -------
         X_pred : list of numpy arrays
         """
-        params = {i: j for i, j in pyro.get_param_store().items()}
+        #params = {i: j for i, j in pyro.get_param_store().items()}
 
         if view is None:
             X_pred = []
             for i in range(len(self.X)):
                 for j in range(len(self.num_factors)):
                     predictive = Predictive(self.sFA_model, guide=self.sFA_guide, num_samples=50, return_sites=[f"X_{i}{j}"])
-                    samples = predictive()
+                    samples = predictive(subsample=0)
                     X_pred.append(np.mean(samples[f"X_{i}{j}"].cpu().numpy(), axis=0))
             return X_pred
         else:
             X_pred = []
             for j in range(self.num_factors):
                 predictive = Predictive(self.sFA_model, guide=self.sFA_guide, num_samples=50, return_sites=[f"X_{view}{j}"])
-                samples = predictive()
+                samples = predictive(subsample=0)
                 X_pred.append(np.mean(samples[f"X_{view}{j}"].cpu().numpy(), axis=0))
             return X_pred
 
